43531a6c8e27bef885ac5d35fbf011f8bde41b3c
eqperes <eqperes@gmail.com>
2015-07-13 15:51:05 +0200

    Update categoryaveraging.py

--- categoryaveraging.py
+++ categoryaveraging.py
@@ -12,3 +12,4 @@ def category_averaging(df, filter_column, filter_value, category_column, value_c
 
 	df_to_set = df[df[filter_column] != filter_value]
 	df.loc[df_to_set.index, value_columns] = averages.loc[df_to_set[category_column]].values
+	
