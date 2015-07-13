29682f8631883ff072d7306cdb9b8ed8d33cbce2
eqperes <eqperes@gmail.com>
2015-07-13 15:50:51 +0200

    Create categoryaveraging.py

---
+++ categoryaveraging.py
@@ -0,0 +1,14 @@
+'''
+Used to obtain averages of some elements in a Pandas DataFrame and then setting other elements with the averages obtained
+'''
+
+import pandas as pd
+
+def category_averaging(df, filter_column, filter_value, category_column, value_columns):
+
+	df_filtered = df[df[filter_column] == filter_value]
+	averages = df_filtered.groupby(category_column).mean()
+	averages = averages.loc[:, value_columns]
+
+	df_to_set = df[df[filter_column] != filter_value]
+	df.loc[df_to_set.index, value_columns] = averages.loc[df_to_set[category_column]].values
