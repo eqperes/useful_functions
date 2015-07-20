9e3d917693f7c2213aaf3403101259afba72e445
eqperes <eqperes@gmail.com>
2015-07-20 13:38:56 +0200

    Update colorplot.py

--- colorplot.py
+++ colorplot.py
@@ -5,6 +5,6 @@ def class_color(classes):
   colors = ["b", "g", "r", "y", "k", "w", "c", "m"]
   return np.array([colors[c] for c in classes])
   
-def plot_color_scatter(X, y, alpha=0.8, s=20):
+def color_scatter(X, y, alpha=0.8, s=20):
   plt.scatter(X[:,0], X[:,1], c=class_color(y), alpha=alpha, s=s)
   plt.show()
